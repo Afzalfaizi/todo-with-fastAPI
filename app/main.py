ed96db43612ce0cfe3286ec9e56a5ae817e79ed4
afzalfaizi <afzaalm993@gmail.com>
2024-04-14 18:19:37 +0500

    Todo App with FastAPI

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 import uvicorn
 
+from app import settings
+
+
 app = FastAPI()
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
 
+@app.get("/db")
+def db_var():
+    return("DB", settings.DATABASE_URL)
 
