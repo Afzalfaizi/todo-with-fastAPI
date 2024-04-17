7123931701b115d9bed27a2f94b87b678e677960
afzalfaizi <afzaalm993@gmail.com>
2024-04-17 21:38:14 +0500

    Todo Server APP

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 import uvicorn
+from sqlmodel import SQLModel, Field
 
 from app import settings
 
+# Step 1: Database Table SCHEMA
+
+class Todo(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    title: str = Field(default=None)
 
 app = FastAPI()
 
