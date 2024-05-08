09732626dcf7ec94f90a72cf4d2ea7936e9cbb27
afzalfaizi <afzaalm993@gmail.com>
2024-05-08 11:34:17 +0500

    full stack todo app

--- app/main.py
+++ app/main.py
@@ -5,33 +5,32 @@ from app import settings
 
 # Create Model
 
-class Todo(SQLModel, table=True):
+class Todo (SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     title : str = Field(min_length=3, max_length=15)
-    description : str = Field(index=True, min_length=4, max_length=60)
     is_completed : bool = Field(default=False)
     
     
 # ENGINE IS ONE TIME IN WHOLE APPLICATION
-connection_string : str = str(settings.DATABASE_URL).replace("postgresql", "postgresql+psycopg")
-engine = create_engine(connection_string, connect_args={"sslmode": "require"}, pool_recycle=300, pool_size=10, echo=True)
+connection_string : str = str(settings.DATABASE_URL).replace("postgresql","postgresql+psycopg")
+engine = create_engine(connection_string, pool_recycle=300, pool_size=10, echo=True)
 
 SQLModel.metadata.create_all(engine)
 
-todo1 : Todo = Todo (description= "First Todo")
-todo2 : Todo = Todo (description= "Second Todo")
-
+todo1 : Todo = Todo (title= "First Todo")
+todo2 : Todo = Todo (title= "Second Todo")
 
 # Session: Separate session for each functionality/transaction
-
 session = Session(engine)
-app: FastAPI = FastAPI()
 
 session.add(todo1)
 session.add(todo2)
 session.commit()
 
 
+
+
+app: FastAPI = FastAPI()
 
 @app.get("/")
 async def root():
