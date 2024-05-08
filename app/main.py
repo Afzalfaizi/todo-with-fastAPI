0221f3323abdd56f0912586bc0c2af72312953c1
afzalfaizi <afzaalm993@gmail.com>
2024-05-08 08:02:17 +0500

    Full Stack Todo App

--- app/main.py
+++ app/main.py
@@ -1,16 +1,21 @@
 from fastapi import FastAPI
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field,create_engine
+from app import settings
 
 
 # Create Model
 
 class Todo(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
-    title : str = Field()
-    description : str = Field()
-    is_completed : bool = Field()
+    id: int | None = Field(default=None, primary_key=True)
+    title : str = Field(min_length=3, max_length=15)
+    description : str = Field(index=True, min_length=4, max_length=60)
+    is_completed : bool = Field(default=False)
+
+connection_string = ''
+engine = create_engine(connection_string)
 
 
+app: FastAPI = FastAPI()
 
 @app.get("/")
 async def root():
